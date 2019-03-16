c081decc21436fd83b1c3034dd291fa9670c220e
Shane Kennedy <shane.kennedy19@gmail.com>
2019-03-16 21:37:42 +0100

    Only add unique songs and remove songs not in yml files

--- src/osp.py
+++ src/osp.py
@@ -6,6 +6,15 @@ import spotipy.util as util
 
 from pprint import pprint
 
+# Environment variables
+SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
+SPOTIFY_USERNAME = os.environ['SPOTIFY_USERNAME']
+
+# Path variables
+base_path = os.getcwd()
+playlists_path = '/playlists/'
+
 
 def get_user_spotify_playlists(client, user):
     playlists = list()
@@ -24,34 +33,68 @@ def get_playlist_by_name(playlist_name, playlists):
     return None
 
 
-# Environment variables
-SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
-SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
-SPOTIFY_USERNAME = os.environ['SPOTIFY_USERNAME']
+def get_playlist_songs(client, playlist_id):
+    response = client.user_playlist(
+        SPOTIFY_USERNAME,
+        playlist_id,
+        fields='tracks')
+    if 'items' in response['tracks']:
+        songs = set([item['track']['uri']
+                     for item in response['tracks']['items']])
+    else:
+        songs = list()
+    return songs
 
-# Path variables
-base_path = os.getcwd()
-playlists_path = '/playlists/'
 
-# Spotify client setup
-token = util.prompt_for_user_token(
-    username=SPOTIFY_USERNAME,
-    scope='playlist-modify-public playlist-modify-private')
-sp = spotipy.Spotify(auth=token)
+def get_songs_to_add(client, playlist_id, song_uris):
+    playlist_song_uris = get_playlist_songs(client, playlist_id)
+    return list(set(song_uris) - playlist_song_uris)
 
-user_playlists_info = get_user_spotify_playlists(sp, SPOTIFY_USERNAME)
-user_playlists_names = [pl['name'] for pl in user_playlists_info]
-playlist_base_name = 'OSP-'
-playlist_files = os.listdir(f'{base_path}{playlists_path}')
 
-for playlist_file in playlist_files:
-    file = open(f'{base_path}{playlists_path}{playlist_file}')
-    playlist_data = yaml.load(file)
-    playlist_name = list(playlist_data)[0]
-    playlist_song_ids = [s['uri'] for s in playlist_data[playlist_name]['songs']]
-    playlist = get_playlist_by_name(playlist_name, user_playlists_info)
-    if not playlist:
-        # Create new playlist and get playlist id
-        playlist = sp.user_playlist_create(SPOTIFY_USERNAME, playlist_name)
-    sp.user_playlist_add_tracks(SPOTIFY_USERNAME, playlist['id'], playlist_song_ids)
+def get_songs_to_delete(client, playlist_id, song_uris):
+    playlist_song_uris = get_playlist_songs(client, playlist_id)
+    return list(playlist_song_uris ^ set(song_uris))
 
+
+def add_songs_to_playlist(client, playlist_id, songs):
+    if len(songs) > 0:
+        client.user_playlist_add_tracks(
+            SPOTIFY_USERNAME,
+            playlist_id,
+            songs)
+
+
+def remove_songs_from_playlist(client, playlist_id, songs):
+    if len(songs) > 0:
+        client.user_playlist_remove_all_occurrences_of_tracks(
+            SPOTIFY_USERNAME,
+            playlist_id,
+            songs)
+
+
+if __name__ == '__main__':
+    # Spotify client setup
+    token = util.prompt_for_user_token(
+        username=SPOTIFY_USERNAME,
+        scope='playlist-modify-public playlist-modify-private')
+    sp = spotipy.Spotify(auth=token)
+
+    user_playlists_info = get_user_spotify_playlists(sp, SPOTIFY_USERNAME)
+    user_playlists_names = [pl['name'] for pl in user_playlists_info]
+    playlist_files = os.listdir(f'{base_path}{playlists_path}')
+
+    for playlist_file in playlist_files:
+        file = open(f'{base_path}{playlists_path}{playlist_file}')
+        playlist_data = yaml.load(file)
+        playlist_name = list(playlist_data)[0]
+        playlist = get_playlist_by_name(playlist_name, user_playlists_info)
+
+        if not playlist:
+            # Create new playlist and get playlist id
+            playlist = sp.user_playlist_create(SPOTIFY_USERNAME, playlist_name)
+
+        playlist_song_uris = [s['uri'] for s in playlist_data[playlist_name]['songs']]
+        songs_to_add = get_songs_to_add(sp, playlist['id'], playlist_song_uris)
+        add_songs_to_playlist(sp, playlist['id'], songs_to_add)
+        songs_to_delete = get_songs_to_delete(sp, playlist['id'], playlist_song_uris)
+        remove_songs_from_playlist(sp, playlist['id'], songs_to_delete)
